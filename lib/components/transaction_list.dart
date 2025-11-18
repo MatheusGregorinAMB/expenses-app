e506590fd71a22b41eee76e0a6190c293d7a7667
MatheusGregorin <suporte2@amb.org.br>
2025-11-18 16:59:09 -0300

    update forma view list

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -24,61 +24,31 @@ class TransactionList extends StatelessWidget {
         itemBuilder: (ctx, index) {
           final tr = transactions[index];
           return Card(
-            child: Row(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(
-                    left: 6,
-                    right: 2,
-                    bottom: 2,
-                    top: 2,
-                  ),
-                  child: Container(
-                    margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: const Color.fromARGB(255, 149, 101, 158),
-                        width: 2,
-                      ),
-                    ),
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      'R\$${tr.value?.toStringAsFixed(2)}',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple,
-                      ),
-                    ),
+            elevation: 5,
+            margin: EdgeInsets.symmetric(
+              vertical: 8,
+              horizontal: 5,
+            ),
+            child: ListTile(
+              leading: CircleAvatar(
+                radius: 30,
+                child: Padding(
+                  padding: const EdgeInsets.all(6.0),
+                  child: FittedBox(
+                    child: Text('R\$${tr.value?.toStringAsFixed(2)}'),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                    left: 12,
-                    right: 2,
-                    bottom: 2,
-                    top: 2,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    spacing: 5,
-                    children: <Widget>[
-                      Text(
-                        tr.title.toString().toUpperCase(),
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        DateFormat('d MMM y').format(tr.date as DateTime),
-                        style: TextStyle(color: Colors.grey[600]),
-                      ),
-                    ],
-                  ),
+              ),
+              title: Text(
+                tr.title ?? '',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
                 ),
-              ],
+              ),
+              subtitle: Text(
+                DateFormat('d MMM y').format(tr.date ?? DateTime.now()),
+              ),
             ),
           );
         },
